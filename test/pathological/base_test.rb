cae8ee129ec3c6c1993368e44b6e8faa4310966c
Caleb Spare <caleb@ooyala.com>
2011-08-15 12:51:49 -0700

    Remove now-unnecessary RR monkey-patch

--- test/pathological/base_test.rb
+++ test/pathological/base_test.rb
@@ -6,40 +6,6 @@ require "stringio"
 
 require "pathological/base"
 
-# Copy-pasted from RR Minitest adapter that I've submitted as a pull request to RR.
-# TODO(caleb): delete
-module RR
-  module Adapters
-    module MiniTest
-      include RRMethods
-      def self.included(mod)
-        RR.trim_backtrace = true
-        mod.class_eval do
-          unless instance_methods.any? { |method_name| method_name.to_sym == :setup_with_rr }
-            alias_method :setup_without_rr, :setup
-            def setup_with_rr
-              setup_without_rr
-              RR.reset
-            end
-            alias_method :setup, :setup_with_rr
-
-            alias_method :teardown_without_rr, :teardown
-            def teardown_with_rr
-              RR.verify
-              teardown_without_rr
-            end
-            alias_method :teardown, :teardown_with_rr
-          end
-        end
-      end
-
-      def assert_received(subject, &block)
-        block.call(received(subject)).call
-      end
-    end
-  end
-end
-
 module Pathological
   class BaseTest < Scope::TestCase
     include RR::Adapters::MiniTest
